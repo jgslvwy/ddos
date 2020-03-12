65f7b3617c2f10b6119efc823a2cc466f08fced7
jingguoshuai <jgs@ccbscf.com>
2020-03-12 22:50:25 +0800

    feat:增加v2版本helloworld

--- src/main/java/com/ccb/dos/controller/HelloWorldController.java
+++ src/main/java/com/ccb/dos/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.ccb.dos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,5 +15,10 @@ public class HelloWorldController {
   @GetMapping("/v1/helloworld")
   public String helloworld() {
     return "hello world!";
+  }
+
+  @PostMapping("/v2/helloworld")
+  public String helloworld(@RequestBody String pk) {
+    return "hello world!" + pk;
   }
 }
---
+++ src/test/java/com/ccb/dos/Test4.java
@@ -0,0 +1,15 @@
+package com.ccb.dos;
+
+/**
+ * @author A
+ * @date 2020/3/12 22:33
+ */
+public class Test4 {
+
+  public static void main(String[] args) {
+    Object o = new Object();
+    Object o1 = new Object();
+    System.out.println(o == o1);
+  }
+
+}
